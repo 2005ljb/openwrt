ca34d2a8a8650d2310067359baa2a92bc5041fc6
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2019-11-27 17:47:40 +0100

    ath10k working with proper caldata and MAC
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- target/linux/ath79/generic/base-files/lib/functions/mikrotik.sh
+++ target/linux/ath79/generic/base-files/lib/functions/mikrotik.sh
@@ -8,13 +8,15 @@ mikrotik_caldata_extract() {
 	local part=$1
 	local offset=$(($2))
 	local count=$(($3))
+	local firmware=$4
 	local mtd
+	local erdfile="/lib/firmware/erd.bin"
 
 	mtd=$(find_mtd_chardev $part)
 	[ -n "$mtd" ] || caldata_die "no mtd device found for partition $part"
 
-	rbextract -e $mtd /lib/firmware/erd.bin
+	rbextract -e $mtd $erdfile
 
-	#dd if=/lib/firmware/silverchis-test-caldata.bin iflag=skip_bytes bs=$count skip=$offset count=1 2>/dev/null || \
-	#	caldata_die "failed to extract calibration data from $mtd"
+	dd if=$erdfile of=/lib/firmware/$firmware iflag=skip_bytes bs=$count skip=$offset count=1 2>/dev/null || \
+		caldata_die "failed to extract calibration data from $mtd"
 }
