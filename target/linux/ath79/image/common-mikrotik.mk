4aef7e6593fe2e8fc9ef9751c47e2e1749052483
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2019-11-28 12:53:01 +0100

    ath79: prepare the target to support MikroTik devices
    This commit adds the basic features for supporting MikroTik devices
    to the ath79 target, namely:
    
     - lzma compression for initramfs images
     - support for minor (MikroTik NOR) split firmware
     - common image parameters
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

---
+++ target/linux/ath79/image/common-mikrotik.mk
@@ -0,0 +1,8 @@
+define Device/mikrotik
+	DEVICE_VENDOR := MikroTik
+	BOARD_NAME := routerboard
+	LOADER_TYPE := elf
+	KERNEL := kernel-bin | append-dtb | lzma | loader-kernel
+	KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | loader-kernel
+	FEATURES += minor
+endef
