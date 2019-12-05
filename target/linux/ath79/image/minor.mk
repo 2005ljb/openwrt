8fb6dec1488585dda0d5125735654ce6d14450b5
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2019-12-05 19:14:22 +0100

    ath79: add support for MikroTik RouterBOARD wAP G-5HacT2HnD
    This commit adds support for the MikroTik RouterBOARD wAP G-5HacT2HnD
    (wAP AC), a small weatherproof dual band, dual-radio 802.11ac
    wireless AP with integrated omnidirectional anntennae and one
    10/100/1000 Mbps Ethernet port.
    
    See https://mikrotik.com/product/RBwAPG-5HacT2HnD for more info.
    
    Specifications:
     - SoC: Qualcomm Atheros QCA9556
     - RAM: 64 MB
     - Storage: 16 MB NOR
     - Wireless:
       · Atheros AR9550 (SoC) 802.11b/g/n 2x2:2, 2 dBi antennae
       · Qualcomm QCA9880 802.11a/n/ac 3x3:3, 2 dBi antennae
     - Ethernet: Atheros AG71xx (SoC, AR8033), 1x 1000/100/10 port,
       passive PoE in
    
    Working:
     - Board/system detection
     - Sysupgrade
     - Serial console
     - Ethernet
     - 2.4 GHz radio
     - 5 GHz radio and LED
     - Reset button
    
    Not working/Unsupported:
     - 2.4 GHz LED
     - AP/CAP LED
     - ZT2046Q SPI temperature and voltage sensor
    
    Acknowledgments: Robert Marko <robimarko@gmail.com>
                     Andrew Cameron <apcameron@softhome.net>
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- target/linux/ath79/image/minor.mk
+++ target/linux/ath79/image/minor.mk
@@ -5,3 +5,16 @@ define Device/mikrotik
 	KERNEL := kernel-bin | append-dtb | lzma | loader-kernel
 	KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | loader-kernel
 endef
+
+define Device/mikrotik_routerboard-wap-g-5hact2hnd
+  $(Device/mikrotik)
+  ATH_SOC := qca9556
+  DEVICE_MODEL := RouterBOARD wAP G-5HacT2HnD (wAP AC)
+  IMAGE_SIZE := 16000k
+  IMAGE/sysupgrade.bin := append-kernel | kernel2minor -s 1024 -e | \
+    pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | \
+    append-metadata | check-size $$$$(IMAGE_SIZE)
+  DEVICE_PACKAGES += kmod-ath10k-ct ath10k-firmware-qca988x-ct
+  SUPPORTED_DEVICES += rb-wapg-5hact2hnd
+endef
+TARGET_DEVICES += mikrotik_routerboard-wap-g-5hact2hnd
